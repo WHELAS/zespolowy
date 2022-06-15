e0ffc4f5215f95700939260dd80095d2ea88d749
WHEALS <filip.karpinski@hotmail.com>
2022-06-15 11:55:14 +0200

    dodawanie

--- zespolowy/Program.cs
+++ zespolowy/Program.cs
@@ -7,16 +7,20 @@ namespace zespolowy
         float Wykonaj(float liczba1, float liczba2);
         
     }
-    //class Dodawanie 
-    ////{
-
-    ////}
+    class Dodawanie : IOperacje
+    {
+        public float Wykonaj(float liczba1, float liczba2)
+        {
+            return liczba1 + liczba2;
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(WypiszImie());
+            Dodawanie d = new Dodawanie();
+            Console.WriteLine(d.Wykonaj(10, 2));
         }
 
         static string WypiszImie()
