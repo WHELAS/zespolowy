0c92854cd1a7f95cd942fcc1d5fbfc6a66765900
dominn44 <domtorres@wp.pl>
2022-06-15 11:57:23 +0200

    .

--- zespolowy/Program.cs
+++ zespolowy/Program.cs
@@ -12,11 +12,19 @@ namespace zespolowy
 
     ////}
 
+    class Mnozenie : IOperacje
+    {
+        public float Wykonaj(float liczba1, float liczba2)
+        {
+            return liczba1 * liczba2;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(WypiszImie());
+            IOperacje m = new Mnozenie();
+            Console.WriteLine(m.Wykonaj(2,4));
         }
 
         static string WypiszImie()
