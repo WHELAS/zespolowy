752ded54f12747e6d94c669b58c276d52ab21fcb
WHEALS <filip.karpinski@hotmail.com>
2022-06-15 11:06:04 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zespolowy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
